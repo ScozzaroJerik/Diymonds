edfb64d1d62b9983f6fb8747b4daab940a63274e
ScozzaroJerik <calogero.scozzaro47@edu.unito.it>
2022-12-01 18:15:41 +0100

    user con nome,cognome,etc...

--- src/main/java/com/javasampleapproach/diymonds/model/User.java
+++ src/main/java/com/javasampleapproach/diymonds/model/User.java
@@ -12,22 +12,50 @@ public class User {
     @Column(name = "username", nullable = false)
     private String username;
 
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "cognome", nullable = false)
+    private String cognome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
     public User() {
     }
 
-    public User(String username) {
+    public User(String username, String nome,String cognome, String email, String password) {
         this.username = username;
+        this.cognome = cognome;
+        this.nome=nome;
+        this.email=email;
+        this.password=password;
     }
 
 
     public long getId() {
         return id;
     }
-
-
     public String getUsername() {
         return username;
     }
+    public String getNome() {
+        return nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+
 
     public void setUsername(String username) {
         this.username = username;
@@ -38,6 +66,10 @@ public class User {
         return "User{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", nome='" + nome + '\'' +
+                ", cog='" + cognome + '\'' +
+                ", psw='" + password + '\'' +
                 '}';
     }
 
